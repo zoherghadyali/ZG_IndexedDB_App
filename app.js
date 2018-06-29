8d29a980c4b1d28d09a3822e975846e5ba842e27
dinopants174 <zoher.ghadyali@gmail.com>
2018-06-29 16:52:09 -0700

    Testing out port in prod

--- app.js
+++ app.js
@@ -10,11 +10,11 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'dist\\public')));
+app.use(express.static(path.join(__dirname, 'dist/public')));
 
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'\\dist\\index.html'));
+    res.sendFile(path.join(__dirname+'/dist/index.html'));
     //__dirname : It will resolve to your project folder.
   });
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
